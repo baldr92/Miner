756d370fdbba1d3586995591ccfbe7646ea19b51
baldr92 <baldr1992@gmail.com>
2017-11-28 16:06:51 +0300

    added code for Rates.Activity

--- app/src/main/java/com/example/user/ethminer/RatesActivity.java
+++ app/src/main/java/com/example/user/ethminer/RatesActivity.java
@@ -10,6 +10,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -22,6 +25,13 @@ public class RatesActivity extends AppCompatActivity {
 
     String url = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,EUR,GBP,CHF,CNY,JPY";
     TextView tv_usd;
+    TextView tv_date;
+    TextView tv_euro;
+    TextView tv_gbp;
+    TextView tv_chf;
+    TextView tv_cny;
+    TextView tv_jpy;
+
 
 
     @Override
@@ -29,14 +39,28 @@ public class RatesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rates);
         tv_usd = findViewById(R.id.usd_rate);
+        tv_date = findViewById(R.id.tv_date);
+        tv_euro = findViewById(R.id.eur_rate);
+        tv_gbp = findViewById(R.id.gbp_rate);
+        tv_chf = findViewById(R.id.chf_rate);
+        tv_cny = findViewById(R.id.cny_rate);
+        tv_jpy = findViewById(R.id.jpy_rate);
+        String date = new SimpleDateFormat("dd.MM.yyyy").format(new Date());
+        tv_date.setText(date);
+
         try {
-            run();
+            runUSD();
+            runEURO();
+            runGBP();
+            runCHF();
+            runCNY();
+            runJPY();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    void run() throws IOException {
+    void runUSD() throws IOException {
         OkHttpClient okHttpClient = new OkHttpClient();
         Request request = new Request.Builder()
                 .url(url)
@@ -56,6 +80,153 @@ public class RatesActivity extends AppCompatActivity {
                         try {
                             JSONObject json = new JSONObject(myResponse);
                             tv_usd.setText(json.getString("USD"));
+                        } catch (JSONException je) {
+                            je.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    void runEURO() throws IOException{
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String myResponse = response.body().string().toString();
+                RatesActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            JSONObject json = new JSONObject(myResponse);
+                            tv_euro.setText(json.getString("EUR"));
+                        } catch (JSONException ex) {
+                              ex.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+
+    void runGBP() throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String myResponse = response.body().string().toString();
+                RatesActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            JSONObject json = new JSONObject(myResponse);
+                            tv_gbp.setText(json.getString("GBP"));
+                        } catch (JSONException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+
+    void runCHF() throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                final String myResponse = response.body().string().toString();
+                RatesActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            JSONObject json = new JSONObject(myResponse);
+                            tv_chf.setText(json.getString("CHF"));
+                        } catch (JSONException je) {
+                            je.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    void runCNY() throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                final String myResponse = response.body().string().toString();
+                RatesActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            JSONObject json = new JSONObject(myResponse);
+                            tv_cny.setText(json.getString("CNY"));
+                        } catch (JSONException je) {
+                            je.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    void runJPY() throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                final String myResponse = response.body().string().toString();
+                RatesActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            JSONObject json = new JSONObject(myResponse);
+                            tv_jpy.setText(json.getString("JPY"));
                         } catch (JSONException je) {
                             je.printStackTrace();
                         }
