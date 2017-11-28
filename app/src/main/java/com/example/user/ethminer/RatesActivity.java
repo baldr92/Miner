7635b20a88475ed73042f32a327fbc5c91454ae0
baldr92 <baldr1992@gmail.com>
2017-11-28 12:39:11 +0300

    added code for Rates.Activity

--- app/src/main/java/com/example/user/ethminer/RatesActivity.java
+++ app/src/main/java/com/example/user/ethminer/RatesActivity.java
@@ -3,13 +3,66 @@ package com.example.user.ethminer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class RatesActivity extends AppCompatActivity {
+
+
+    String url = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,EUR,GBP,CHF,CNY,JPY";
+    TextView tv_usd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rates);
+        tv_usd = findViewById(R.id.usd_rate);
+        try {
+            run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void run() throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                final String myResponse = response.body().string().toString();
+                RatesActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            JSONObject json = new JSONObject(myResponse);
+                            tv_usd.setText(json.getString("USD"));
+                        } catch (JSONException je) {
+                            je.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
     }
 
     public void onClickBac(View view ) {
